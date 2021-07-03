f05e38d357fdea65836a2fc02e28eb39176c5bac
buraiang99 <37676815+buraiang99@users.noreply.github.com>
2021-07-03 15:23:21 -0600

    Update footer.php

--- public/footer.php
+++ public/footer.php
@@ -2,6 +2,30 @@
 <script src="../public/js/bootstrap.min.js"></script>
 <script src="../public/js/funciones.js"></script>
 
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.7.0/firebase-app.js%22%3E</script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.7.0/firebase-analytics.js%22%3E</script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDZdpuUCa2FURbunP1aTnPb0ZeqhAeFDko",
+    authDomain: "implementacionejemplo-803ec.firebaseapp.com",
+    projectId: "implementacionejemplo-803ec",
+    storageBucket: "implementacionejemplo-803ec.appspot.com",
+    messagingSenderId: "474275368315",
+    appId: "1:474275368315:web:921ec4309f763dd64941de",
+    measurementId: "G-KSC8J70SFJ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
+
 <footer class="bg-dark text-white py-4">
     <div class="container text-md-left">
         <nav class="row">
