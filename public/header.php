61bddece1c26db2ec7d9fcea4db4226239f3b723
buraiang99 <37676815+buraiang99@users.noreply.github.com>
2021-07-03 15:27:13 -0600

    Update header.php

--- public/header.php
+++ public/header.php
@@ -20,6 +20,30 @@
         
     </head>
     <body>
+                   <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.7.0/firebase-app.js%22%3E</script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.7.0/firebase-analytics.js%22%3E</script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyDZdpuUCa2FURbunP1aTnPb0ZeqhAeFDko",
+    authDomain: "implementacionejemplo-803ec.firebaseapp.com",
+    projectId: "implementacionejemplo-803ec",
+    storageBucket: "implementacionejemplo-803ec.appspot.com",
+    messagingSenderId: "474275368315",
+    appId: "1:474275368315:web:921ec4309f763dd64941de",
+    measurementId: "G-KSC8J70SFJ"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
+                   <!--F-->
         <!--Navbar-->
         <nav class="navbar navbar-expand-sm navbar-dark bg-danger">
             <div class="container-fluid">
