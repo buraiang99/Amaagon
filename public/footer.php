7bc6ec0d83c59142e3ead7fee2ac4a5e4d27ae82
buraiang99 <37676815+buraiang99@users.noreply.github.com>
2021-07-03 15:25:13 -0600

    Update footer.php

--- public/footer.php
+++ public/footer.php
@@ -2,6 +2,24 @@
 <script src="../public/js/bootstrap.min.js"></script>
 <script src="../public/js/funciones.js"></script>
 
+
+
+<footer class="bg-dark text-white py-4">
+    <div class="container text-md-left">
+        <nav class="row">
+        <div class="col-lg-3 col-md-3 col-sm-12">
+        <a href="#" >
+                <img src="../public/img/logoUCR.png" alt="Logo" class="img-logo mr-2" width="200" height="100">
+            </a>
+        </div>
+            <div class="col-lg-6 col-md-9 col-sm-12 text-center pt-3">
+                <h6>Bryan Soto Ureña</h6>
+                <h6>Esta página es solo para uso académico</h6>
+            </div>
+        </nav>
+    </div>
+</footer>
+</body>
 <!-- The core Firebase JS SDK is always required and must be listed first -->
 <script src="https://www.gstatic.com/firebasejs/8.7.0/firebase-app.js%22%3E</script>
 
@@ -25,22 +43,4 @@
   firebase.initializeApp(firebaseConfig);
   firebase.analytics();
 </script>
-
-<footer class="bg-dark text-white py-4">
-    <div class="container text-md-left">
-        <nav class="row">
-        <div class="col-lg-3 col-md-3 col-sm-12">
-        <a href="#" >
-                <img src="../public/img/logoUCR.png" alt="Logo" class="img-logo mr-2" width="200" height="100">
-            </a>
-        </div>
-            <div class="col-lg-6 col-md-9 col-sm-12 text-center pt-3">
-                <h6>Bryan Soto Ureña</h6>
-                <h6>Esta página es solo para uso académico</h6>
-            </div>
-        </nav>
-    </div>
-</footer>
-</body>
-
 </html>
